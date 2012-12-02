56655a9dc5094df6e3418281e1c9a3d065dd96b0
JStyleConcept <jstyleconcept@gmail.com>
2012-12-03 02:39:14 +0800

    2012/12/03 Add view source code option
    Add view source code option

---
+++ src/net/jstyleconcept/android/helloworld/SourceCodeActivity.java
@@ -0,0 +1,19 @@
+package net.jstyleconcept.android.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.webkit.WebView;
+
+public class SourceCodeActivity extends Activity {
+
+	WebView sourceCodeView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_source_code);
+		sourceCodeView=(WebView)this.findViewById(R.id.webView1);
+		sourceCodeView.getSettings().setBuiltInZoomControls(true);
+		sourceCodeView.loadUrl("file:///android_asset/sourcecode.html");
+	}
+}
